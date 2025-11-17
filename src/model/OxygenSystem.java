212b7085503be7b3a126ac1c46b8d7f12f20a5aa
Gcbens <gvolmar82@gmail.com>
2025-11-17 00:29:35 -0500

    Final Item and OxygenSystem classes

--- src/model/OxygenSystem.java
+++ src/model/OxygenSystem.java
@@ -1,6 +1,5 @@
 package model;
 
-<<<<<<< HEAD
 // Created by: Eyob
 // This class handles all oxygen behavior in the game.
 // It tracks current oxygen, draining, restoring, and checking if oxygen is critical.
@@ -13,22 +12,6 @@ public class OxygenSystem {
     private int depletionRate;
 
     // constructor sets starting oxygen and how fast it drains
-=======
-// represents the player's oxygen management system
-// keeps track of how much oxygen the player has left
-// reduces oxygen as time passes or as the player moves between rooms
-public class OxygenSystem {
-
-    // this variable represents the current oxygen level (out of 100)
-    private int oxygenLevel;
-
-    // this variable sets how much oxygen is lost each turn or movement
-    private int depletionRate;
-
-    // constructor for creating an oxygen system
-    // startingLevel = starting oxygen percentage
-    // depletionRate = how much oxygen is lost each move
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public OxygenSystem(int startingLevel, int depletionRate) {
         this.oxygenLevel = startingLevel;
         this.depletionRate = depletionRate;
@@ -39,11 +22,7 @@ public class OxygenSystem {
         return oxygenLevel;
     }
 
-<<<<<<< HEAD
     // this method reduces oxygen by the normal depletion rate
-=======
-    // decreases oxygen by the set depletion rate
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public void decreaseOxygen() {
         oxygenLevel -= depletionRate;
         if (oxygenLevel < 0) {
@@ -51,12 +30,7 @@ public class OxygenSystem {
         }
     }
 
-<<<<<<< HEAD
     // this method reduces oxygen by a specific amount
-=======
-    // decreases oxygen by a custom amount
-    // amount = how much oxygen to remove
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public void decreaseOxygenBy(int amount) {
         oxygenLevel -= amount;
         if (oxygenLevel < 0) {
@@ -64,12 +38,7 @@ public class OxygenSystem {
         }
     }
 
-<<<<<<< HEAD
     // this method restores oxygen by a specific amount
-=======
-    // restores oxygen by a custom amount
-    // amount = how much oxygen to add
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public void restoreOxygen(int amount) {
         oxygenLevel += amount;
         if (oxygenLevel > 100) {
@@ -77,7 +46,6 @@ public class OxygenSystem {
         }
     }
 
-<<<<<<< HEAD
     // this method restores oxygen when using an oxygen item
     public void useOxygenItem(int amount) {
         restoreOxygen(amount);
@@ -87,40 +55,23 @@ public class OxygenSystem {
     }
 
     // this method checks if oxygen is dangerously low
-=======
-    // checks if oxygen is at a critical level (20% or lower)
-    // return = true if oxygen is dangerously low
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public boolean isCritical() {
         return oxygenLevel <= 20;
     }
 
-<<<<<<< HEAD
     // this prints the current oxygen level to the player
     public void showOxygenStatus() {
         if (isCritical()) {
             System.out.println("Warning. Oxygen level critical (" + oxygenLevel + "%). Find oxygen soon.");
-=======
-    // displays a warning message if oxygen is low
-    public void showOxygenStatus() {
-        if (isCritical()) {
-            System.out.println("Warning: Oxygen level critical (" + oxygenLevel + "%). Find oxygen soon!");
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
         } else {
             System.out.println("Oxygen level: " + oxygenLevel + "%");
         }
     }
-<<<<<<< HEAD
     public void setOxygenLevel(int lvl) {
         this.oxygenLevel = lvl;
     }
 
     // this checks if the player still has oxygen left
-=======
-
-    // checks if player can survive (oxygen above zero)
-    // return = true if oxygen left, false if out
->>>>>>> 38f27ce9fdf6b988ece66d4e212656444e23ad5d
     public boolean canBreathe() {
         return oxygenLevel > 0;
     }
